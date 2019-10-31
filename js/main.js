e81f8d5275e5e3e1c333ba7dcc7b70593c670c6f
jmia <jen.armstrong89@gmail.com>
2019-10-31 15:21:00 -0400

    added map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready( function () {
+    // Global variables
+    var map;
+    
+    // Setup jQuery DataTables
     $('#mapTable').DataTable();
+    
+    // Setup Bing Maps
+    function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('bingMap'), {});
+
+    loadMapScenario();
+}
 } );
